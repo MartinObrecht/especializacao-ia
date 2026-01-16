46ebe556fa6768c315b6b9049a6d986c81c071d4
MartinObrecht <martin.obrecht@yahoo.com.br>
2026-01-16 16:52:19 -0300

    Fundamentos de Gen Ai

---
+++ llm-01.py
@@ -0,0 +1,14 @@
+from google import genai
+from google.genai import types
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    config=types.GenerateContentConfig(
+        system_instruction="Atue como um especialista em machine learning."
+    ),
+    contents="Explique de forma resumida como a IA funciona",
+)
+print(response.text)
